d5ab7e860b760b3a95fd93f2c19281aaecfcb7ec
calebmor16-alt <calebmor10@gmail.com>
2025-12-30 22:38:56 -0600

    Update opengraph-image.tsx

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from 'next/og';
 
-export const runtime = 'edge';
+
 export const alt = 'Moderntology - Advanced Astrology & Numerology';
 export const size = {
     width: 1200,
